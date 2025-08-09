c03e9e5acef31b3a13a02daa9be0b20fb7f7cac8
stefan-galca <galcastefan2000@gmail.com>
2025-08-09 12:04:07 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// sw.js — v8
-const CACHE = 'fuel-pwa-v8';
+// sw.js — v9
+const CACHE = 'gse-fuel-pwa-v9';
 const ASSETS = ['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));});
